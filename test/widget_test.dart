1a961aeafb5cfaf022f14a0efd4be7f78463935f
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-07-20 23:53:01 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:adaptive_app/src/playlists.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -14,7 +13,7 @@ import 'package:adaptive_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Playlists());
+    await tester.pumpWidget(const PlaylistsApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
